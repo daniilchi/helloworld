eb93b90be8c395291e8c7f8f157dd980fe13d7b3
daniil <tushind1@gmail.com>
2025-02-13 14:43:20 +0300

    test commit

--- main.go
+++ main.go
@@ -4,5 +4,19 @@ import "fmt"
 
 // test
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println(HelloWorld())
+	fmt.Println(SecondString())
+	fmt.Println(ThirdString())
+}
+
+func HelloWorld() string {
+	return "Hello World!"
+}
+
+func SecondString() string {
+	return "This is second line!"
+}
+
+func ThirdString() string {
+	return "This is third line!"
 }
